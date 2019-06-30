3adaf06cfe812a57e56e190fbd24816b97950dd4
kenlwk <liwaikit1018@gmail.com>
2019-06-30 12:08:27 +0800

    added Fan-menu

--- Example/AnalysisController.swift
+++ Example/AnalysisController.swift
@@ -5,48 +5,11 @@ import Macaw
 
 class AnalysisViewController: UIViewController {
     
-    @IBOutlet weak var fanMenu: FanMenu!
-    @IBOutlet weak var topView: UIView!
+    //@IBOutlet weak var fanMenu: FanMenu!
+   // @IBOutlet weak var topView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        fanMenu.button = FanMenuButton(
-            id: "main",
-            image: UIImage(named: "menu_plus"),
-            color: Color(val: 0xADADAD)
-        )
-        
-        fanMenu.items = [
-            FanMenuButton(
-                id: "photo",
-                image: UIImage(named: "shop_photo"),
-                color: Color(val: 0xCECBCB)
-            ),
-            FanMenuButton(
-                id: "gallery",
-                image: UIImage(named: "shop_gallery"),
-                color: Color(val: 0xCECBCB)
-            ),
-        ]
-        
-        fanMenu.menuRadius = 100.0
-        fanMenu.duration = 0.2
-        fanMenu.interval = (Double.pi + Double.pi/4, Double.pi + 3 * Double.pi/4)
-        fanMenu.radius = 25.0
-        fanMenu.delay = 0.0
-        
-        fanMenu.onItemWillClick = { button in
-            self.showView()
-        }
-        
-        fanMenu.backgroundColor = .clear
-    }
-    
-    func showView() {
-        let newValue: CGFloat = self.topView.alpha == 0.0 ? 1.0 : 0.0
-        UIView.animate(withDuration: 0.35, animations: {
-            self.topView.alpha = newValue
-        })
     }
 }
--- Example/HomeViewController.swift
+++ Example/HomeViewController.swift
@@ -3,22 +3,14 @@ import UIKit
 import FanMenu
 import Macaw
 
-class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class HomeViewController: UIViewController {
     
     @IBOutlet weak var fanMenu: FanMenu!
-    @IBOutlet weak var tableView: UITableView!
+    //@IBOutlet weak var tableView: UITableView!
     
-    let data = [
-        ("income", "Salary", "06-01", "+$1353.00", "transaction_salary"),
-        ("outcome", "Coffee", "06-02", "-$3.50", "transaction_coffee"),
-        ("income", "Part Time", "06-02", "+$100.00", "transaction_clock"),
-        ("outcome", "Dinner", "06-04", "-$55.00", "transaction_dinner"),
-        ("outcome", "Shopping", "06-04", "-$511.30", "transaction_shoping"),
-        ("outcome", "Travel", "07-05", "-$2800.00", "transaction_travel")
-    ]
     
     let items = [
-        ("exchange", 0x9F85FF),
+        ("camera", 0x9F85FF),
         ("wallet", 0x85B1FF),
         ("money_box", 0xFF703B),
         ("visa", 0xF55B58),
@@ -27,14 +19,6 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        /*tableView.dataSource = self
-        tableView.delegate = self
-        tableView.separatorColor = UIColor(
-            red: CGFloat(211.0) / 255.0,
-            green: CGFloat(223.0) / 255.0,
-            blue: CGFloat(242.0) / 255.0,
-            alpha: 1.0
-        ) */
         
         fanMenu.button = FanMenuButton(
             id: "main",
@@ -72,25 +56,15 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         fanMenu.updateNode()
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let data = self.data[indexPath.row]
-        let cellClass = data.0 == "income" ? "IncomeCell" : "OutcomeCell"
-        let transactionCell = self.tableView.dequeueReusableCell(withIdentifier: cellClass) as! TransactionCell
-        transactionCell.titleLabel.text = data.1
-        transactionCell.dateLabel.text = data.2
-        transactionCell.amountLabel.text = data.3
-        transactionCell.transactionImage.image = UIImage(named: data.4)!
-        return transactionCell
-    }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
-    }
+
 }
 
+/*
 class TransactionCell: UITableViewCell {
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var amountLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var transactionImage: UIImageView!
 }
+*/
