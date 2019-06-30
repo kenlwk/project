deba5dd35e1fcb19c0a99276fe8eedf520d52756
kenlwk <liwaikit1018@gmail.com>
2019-06-30 16:05:13 +0800

    updated UI intro

---
+++ SmartVeggie/IntroViewController.swift
@@ -0,0 +1,22 @@
+//
+//  IntroViewController.swift
+//  SmartVeggie
+//
+//  Created by Ken Li on 30/6/2019.
+//  Copyright © 2019 Exyte. All rights reserved.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+
+
+}
